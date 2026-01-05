766e606b982cbe62b68871625245f4dbe6c1f980
arshdeepsingh157 <74786642+arshdeepsingh157@users.noreply.github.com>
2026-01-05 15:18:44 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+# Fake database
+EMPLOYEES = {
+    "A1B2C3D4": "ALLOW",
+    "11112222": "DENY"
+}
+
+@app.route("/attendance", methods=["POST"])
+def attendance():
+    data = request.json
+    uid = data.get("rfid_uid")
+
+    result = EMPLOYEES.get(uid, "DENY")
+
+    return jsonify({
+        "result": result
+    })
+
+if __name__ == "__main__":
+    app.run()
